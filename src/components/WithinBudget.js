752129ac78620a57c85727a08a7397277249dd77
Vivek Jadhav <vivek.jadhav200127@gmail.com>
2025-10-30 20:07:07 +0530

    thead tag and css stylings

--- src/components/WithinBudget.js
+++ src/components/WithinBudget.js
@@ -1,46 +1,54 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 
 function WithinBudget() {
-    const items = [
-        { name: "Apple", price: 20 },
-        { name: "Banana", price: 10 },
-        { name: "Neutella", price: 30 },
-        { name: "Razor", price: 5 },
-        { name: "CornFlakes", price: 15 },
-        { name: "Sound Bar", price: 50 },
-        { name: "Iphone", price: 80 },
-    ];
+  const items = [
+    { name: "Apple", price: 20 },
+    { name: "Banana", price: 10 },
+    { name: "Neutella", price: 30 },
+    { name: "Razor", price: 5 },
+    { name: "CornFlakes", price: 15 },
+    { name: "Sound Bar", price: 50 },
+    { name: "Iphone", price: 80 },
+  ];
 
-    const [budget,setBudget] = useState("")
-    return (
-        <div className='p-14'>
-            <h2 className='font-bold'>Enter your budget to check avaliable items:</h2>
+  const [budget, setBudget] = useState("");
 
-            <input
-            type='number'
-            value={budget}
-            onChange={(e)=>setBudget(e.target.value)}
-            />
+  return (
+    <div>
+      <h2>Enter your budget to check available items:</h2>
+      <input
+        type="number"
+        value={budget}
+        onChange={(e) => setBudget(e.target.value)}
+      />
+      <h3>Items you can buy are in Green color</h3>
 
-            <h3>Items you can buy are in green color</h3>
-            <table>
-                <tbody>
-          {items.map((item) => (
-            <tr key={item.name}>
-              <td>
-                {item.name}
-              </td>
-              <td
-                className={`${Number(budget)>=item.price ? "text-green-600" : "text-red-600"}`}
-              >
-                {item.price}
-              </td>
-            </tr>
-          ))}
+      <table border="1">
+        <thead>
+          <tr>
+            <th>Item</th>
+            <th>Price</th>
+          </tr>
+        </thead>
+        <tbody>
+          {items.map((item) => {
+            const affordable = Number(budget) >= item.price;
+            const color = budget
+              ? affordable
+                ? "green"
+                : "red"
+              : "black"; // default black
+            return (
+              <tr key={item.name}>
+                <td style={{ color }}>{item.name}</td>
+                <td style={{ color }}>{item.price}</td>
+              </tr>
+            );
+          })}
         </tbody>
-            </table>
-        </div>
-    )
+      </table>
+    </div>
+  );
 }
 
-export default WithinBudget
+export default WithinBudget;
